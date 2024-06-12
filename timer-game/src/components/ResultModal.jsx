3ee6c41ec554d48d0b34c7bee8c4e68583329af4
Kiran9223 <88050617+Kiran9223@users.noreply.github.com>
2024-06-12 17:12:02 +0530

    added portal

--- timer-game/src/components/ResultModal.jsx
+++ timer-game/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset}, ref) {
     
@@ -16,7 +17,8 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
         }
     });
     
-    return (
+    return createPortal(
+    
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
         {userLost && <h2>Your Lost Y_Y </h2>}
         {!userLost && <h2>Your Score {score} </h2> }
@@ -25,7 +27,8 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
         <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
     );
 })
 
